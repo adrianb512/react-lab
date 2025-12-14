060b951370cdbac63625ac65affd5b35484847b8
Adrian <test@test.pl>
2025-12-14 14:44:43 +0100

    warunkilogiczne

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
   
-         let title = 'Wall-E';
+        // let title = 'Wall-E';
+          const [title, setTitle] = useState('Wall-E');
 
+          const [info, setInfo] = useState('');
     function handleChange(event) {
-        console.log(event.target.value);
+        //console.log(event.target.value);
+        setTitle(event.target.value);
+
+        if(event.target.value.length>10)
+        {
+           setInfo('Tytul jest za dlugi');
+        }
+        else if(event.target.value.length>5)
+        {
+          
+          setInfo('Tytul jest ok ');
+        }
+       
+       else if(event.target.value.length>0)
+       {
+        setInfo('Tytul jest za krotki ');
+       }else
+        setInfo('');
+          
         
     }
 
@@ -14,7 +35,9 @@ function App() {
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>{info}</h2>
+        
+            <input type="text" value={title} onChange={handleChange}/>
         </div>
     
     
