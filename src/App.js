2d44fd038ddb313426055c4c04b2f8b933d08323
Adrian <test@test.pl>
2025-12-14 15:06:32 +0100

    listy

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
 
           const [info, setInfo] = useState('');
 
+          const movies = [
+              {title: "Wall-E"},
+              {title: "Pulp Fiction"},
+              {title: "Matrix"},
+              {title: "1670"},
+          ];
+
+
        function pokazalert(event) {
           alert(title);
         }
@@ -37,13 +45,35 @@ function App() {
        
         
     }
+    //druga metoda :
+    let message='';
+    if(title.length>10)
+        {
+           message='Tytul jest za dlugi';
+        }
+        else if(title.length>5)
+        {
+          
+         message='Tytul jest ok';
+        }
+       
+       else if(title.length>0)
+       {
+        message='Tytul jest za krotki';
+       }else
+        message='';
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <h2>{info}</h2>
-        
+            <h3>Title</h3>
+            <ul>
+                {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+            </ul>
+
+              <p>{info}</p>
+              <p>{message}</p>
             <input type="text" value={title} onChange={handleChange}/>
             <button type="submit" onClick={pokazalert}>Pokaz tytul filmu</button>
         </div>
