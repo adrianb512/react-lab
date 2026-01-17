003e7d7ecf6f8084f6b3593c570a7f9f9af3a9ee
Adrian <adrianbober@gmail.com>
2026-01-17 14:21:51 +0100

    Dodawaniefilmow

--- src/App.js
+++ src/App.js
@@ -1,43 +1,102 @@
+
 import './App.css';
 import {useState} from "react";
 import "milligram";
-
 function App() {
-    const [title, setTitle] = useState('Wall-E');
+  
 
-    let message;
-    if (title.length < 5) {
-        message = <div>Tutuł jest za krótki. Nagrywają takie filmy?</div>;
-    } else if (title.length < 15) {
-        message = <div>Tytuł jest ekstra, w sam raz na plakat przed kinem!</div>;
-    } else {
-        message = <div>Tytuł jest za długi, nikt tego nie zapamięta.</div>;
-    }
+          const [title, setTitle] = useState('');
+          const [year, SetYear] = useState('');
 
+          const [movies, setMovies] = useState([
+              {title: "Wall-E",year:2022},
+              {title: "Pulp Fiction",year:2022},
+              {title: "Matrix",year:2022},
+              {title: "1670",year:2022},
+          ]);
+
+
+       function dodajfilm(event) {
+          
+        if(message != 'Tytul jest ok')
+        {
+          alert(message);
+        }else
+                if(year.length<1)
+           alert('brak podanie roku');
+
+          else
+          {
+            const newMovie = [
+                          {title: title,rok:year}
+                          ];
+
+            setMovies([...movies, ...newMovie]);
+
+            SetYear('');
+
+            setTitle('');
+
+
+
+          }
+      
+      }
+        
     function handleChange(event) {
+        //console.log(event.target.value);
         setTitle(event.target.value);
     }
 
-    const movies = [
-        {title: "Wall-E"},
-        {title: "Pulp Fiction"},
-        {title: "Matrix"},
-        {title: "1670"},
-    ];
+
+
+    let message='';
+    if(title.length>10)
+        {
+           message='Tytul jest za dlugi';
+        }
+        else if(title.length>5)
+        {
+          
+         message='Tytul jest ok';
+        }
+       
+       else if(title.length>0)
+       {
+        message='Tytul jest za krotki';
+       }else
+        message='';
 
     return (
-        <div className="container">
+        <div className="container"> 
             <h1>My favourite movies to watch</h1>
-            <h2>Titles</h2>
+
+            <h3>Title</h3>
             <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title}</li>)}
+                {movies.map((movie) => <li key={movie.title}>{movie.title} ({movie.rok})</li>)}
             </ul>
-            <h2>My favourite movie for today is {title}</h2>
-            {title.length > 0 && <div>{message}</div>}
-            <input type="text" value={title} onChange={handleChange}/>
-            <button onClick={() => alert(title)}>Pokaż tytuł filmu</button>
+
+           
+          <h2>Add movie </h2>
+
+            <label>Tytul </label>
+
+          {
+            title.length > 0 && <div>{message}</div>
+
+          }
+
+            <input type="text" value={title} onChange={handleChange} />
+
+            <label>Rok</label>
+
+            <input type="number"  value={year}  onChange={(event=>SetYear(event.target.value))} />
+
+            <button type="submit" onClick={dodajfilm}>Dodaj film</button>
         </div>
-    );
+    
+    
+        ); 
 }
 
 export default App;
